113c18ddeb3f5afca58f73fc5b2b7d8820f274dc
James Moore <moorejames099@gmail.com>
2016-01-21 18:28:29 -0700

    Initial Commit, Base project

---
+++ src/config.h
@@ -0,0 +1,18 @@
+/*
+ * config.h
+ *
+ *  Created on: Jan 16, 2016
+ *      Author: James
+ */
+
+#ifndef SRC_CONFIG_H_
+#define SRC_CONFIG_H_
+//note: all motor channels labeled right or left are from the robot's perspective
+#define RIGHT_REAR_MOTOR_CHANNEL 0
+#define RIGHT_FRONT_MOTOR_CHANNEL 1
+#define LEFT_REAR_MOTOR_CHANNEL 2
+#define LEFT_FRONT_MOTOR_CHANNEL 3
+
+
+
+#endif /* SRC_CONFIG_H_ */
