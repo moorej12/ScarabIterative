3dbe9c14f50d573aaf129f10347852fc734764ec
sethhinz <sethhinz@me.com>
2016-01-23 15:30:27 -0700

    Ported arms to prototype because everyone is lazy but me who is super coolgit statusgit statusgit status

--- src/Arms.cpp
+++ src/Arms.cpp
@@ -8,65 +8,29 @@
 #include "Arms.h"
 #include "config.h"
 
-class Arms {
-	private:
+Arms::Arms() {
+	m_leftArmsMotor = new Victor(LEFT_ARMS_MOTOR_CHANNEL);
+	m_rightArmsMotor = new Victor(RIGHT_ARMS_MOTOR_CHANNEL);
+	m_armAngle = 0;
+}
 
-		//make sure to update the arm angle every cycle...
-		float *m_armAngle;
-		Encoder *m_leftEncoder;
-		Encoder *m_rightEncoder;
-		float *m_targetArmsAngle;
-		VictorSP *m_leftArmsMotor;
-		VictorSP *m_rightArmsMotor;
+Arms::~Arms() {
+	delete m_leftArmsMotor;
+	delete m_rightArmsMotor;
+	delete m_armAngle;
+}
 
-		//Checks if the arms are at the minimum possible angle.
-		//If true, it doesn�t allow further movement in that direction.
+//Sets arm position based on value in degrees
+void Arms::SetPosition(float m_setArmAngle) {
 
-		bool IsLowestPosition() {
-			return false;
-		}
+}
 
-		//Checks if the arms are at the maximum possible angle.
-		//If true, it doesn�t allow further movement in that direction.
+//Allows driver to move arms manually
+void Arms::Move() {
 
-		bool IsHighestPosition() {
-			return false;
-		}
+}
 
-
-	public:
-		Arms::Arms() {
-<<<<<<< HEAD
-			m_leftArmsMotor = new Victor(LEFT_ARMS_MOTOR_CHANNEL);
-			m_rightArmsMotor = new Victor(RIGHT_ARMS_MOTOR_CHANNEL);
-			m_armAngle = 0;
-=======
-			m_leftArmsMotor = new Jaguar(LEFT_ARMS_MOTOR_CHANNEL);
-			m_rightArmsMotor = new Jaguar(RIGHT_ARMS_MOTOR_CHANNEL);
-			float m_armAngle = 0;
-			float m_setArmAngle = 0;
->>>>>>> 8ae0bf2b774e67d41f26dbdb6956a1f440ab7fab
-		}
-		Arms::~Arms() {
-			delete m_leftArmsMotor;
-			delete m_rightArmsMotor;
-			delete m_armAngle;
-			delete m_setArmAngle;
-		}
-		//Sets arm position based on value in degrees
-		void SetPosition(float m_setArmAngle) {
-
-		}
-
-		//Allows driver to move arms manually
-		void Move() {
-
-		}
-
-		//Gets the angle of the arms from the potentiometer
-		float GetPosition() {
-			return m_armAngle;
-		}
-
-
-};
+//Gets the angle of the arms from the potentiometer
+float Arms::GetPosition() {
+	return m_armAngle;
+}
--- src/Arms.h
+++ src/Arms.h
@@ -9,9 +9,21 @@
 #define SRC_ARMS_H_
 
 class Arms {
+private:
+	float *m_armAngle;
+	Encoder *m_leftEncoder;
+	Encoder *m_rightEncoder;
+	float *m_targetArmsAngle;
+	VictorSP *m_leftArmsMotor;
+	VictorSP *m_rightArmsMotor;
+	bool IsLowestPosition();
+	bool IsHighestPosition();
 public:
 	Arms();
-	virtual ~Arms();
+	~Arms();
+	void SetPosition(float m_setArmAngle);
+	void Move();
+	float GetPosition();
 };
 
 #endif /* SRC_ARMS_H_ */
